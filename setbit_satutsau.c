d81126b0e2d9895eec933411e7f5eb41cf1f3c8c
fakhruzzaman <faysal_raul@yahoo.com>
2013-11-07 17:16:50 +0700

    Create setbit_satutsau.c

---
+++ setbit_satutsau.c
@@ -0,0 +1,26 @@
+/* SET BIT SATU-SATU */
+
+#include <mega8535.h>
+
+void main(void)
+{
+	DDRA.0 = 1;
+    DDRA.1 = 1;
+    DDRA.2 = 1;
+    DDRA.3 = 1;
+    DDRA.4 = 1;
+    DDRA.5 = 1;
+    DDRA.6 = 1;
+    DDRA.7 = 1;
+    
+    PORTA.0 = 1;
+    PORTA.1 = 1;
+    PORTA.2 = 1;
+    PORTA.3 = 1;
+    PORTA.4 = 1;
+    PORTA.5 = 1;
+    PORTA.6 = 1;
+    PORTA.7 = 1;
+    
+    
+}
